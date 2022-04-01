7a63ffd9e545caf0d392a111a4fc42257618e2cb
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-04-01 12:42:37 -0500

    Se pretende mejorar el diseño del sitio web, se ah agregado imagenes a la pagina principal

--- Sistema-Lince/resources/views/client/home.blade.php
+++ Sistema-Lince/resources/views/client/home.blade.php
@@ -8,11 +8,11 @@
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="{{ asset('img/home/presentation1.jpg') }}" class="d-block w-100 img-presentation" alt="foto">
+            <div class="presentation1 carousel-item active">
+                <img src="{{ asset('img/home/presentation1.jpg') }}" class="d-block h-100 w-auto img-presentation" alt="Imagen de Darkmoon_Art en Pixabay">
             </div>
-            <div class="carousel-item">
-            <img src="{{ asset('img/home/presentation2.jpg') }}" class="d-block w-100 img-presentation" alt="...">
+            <div class="presentation2 carousel-item">
+                <img src="{{ asset('img/home/presentation2.jpg') }}" class="d-block h-100 w-auto img-presentation" alt="Imagen de Pexels en Pixabay">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -27,30 +27,30 @@
 </section>
 <div class="row mt-5">
     <div class="col-md-6">
-        <div class="card bg-dark text-white" style="height: 100px;">
-            <a href="#"><img src="..." class="card-img" alt="..."></a>
+        <div class="card text-white">
+            <a href="#"><img src="{{ asset('img/home/categorie-living-room.jpg') }}" class="card-img" alt="Imagen de StockSnap en Pixabay"></a>
         </div>
     </div>
     <div class="col-md-6">
-        <div class="card bg-dark text-white" style="height: 100px;">
-            <a href="#"><img src="..." class="card-img" alt="..."></a>
+        <div class="card text-white">
+            <a href="#"><img src="{{ asset('img/home/categorie-dining-room.jpg') }}" class="card-img" alt="Imagen de ErikaWittlieb en Pixabay"></a>
         </div>
     </div>
 </div>
 <div class="row my-3">
     <div class="col-md-4">
-        <div class="card bg-dark text-white" style="height: 100px;">
-            <a href="#"><img src="..." class="card-img" alt="..."></a>
+        <div class="card text-white">
+            <a href="#"><img src="{{ asset('img/home/categorie-bedroom.jpg') }}" class="card-img" alt="Imagen de khiem tran en Pixabay "></a>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="card bg-dark text-white" style="height: 100px;">
-            <a href="#"><img src="..." class="card-img" alt="..."></a>
+        <div class="card text-white">
+            <a href="#"><img src="{{ asset('img/home/categorie-ofice.jpg') }}" class="card-img" alt="Imagen de ErikaWittlieb en Pixabay "></a>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="card bg-dark text-white" style="height: 100px;">
-            <a href="#"><img src="..." class="card-img" alt="..."></a>
+        <div class="card text-white">
+            <a href="#"><img src="{{ asset('img/home/categorie-electr.jpg') }}" class="card-img" alt="Imagen de JamesDeMers en Pixabay "></a>
         </div>
     </div>
 </div>
